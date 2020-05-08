4af167ec56ddf11a103428330866a662ad902baa
shuibakhthar <59691986+shuibakhthar@users.noreply.github.com>
2020-05-08 18:13:01 +0530

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -1,6 +1,8 @@
 const mnav = document.getElementsByClassName("mobile-nav");
 const togglenav = document.getElementsByClassName("toggle-nav");
-
+const marvel = document.getElementsByClassName("marvel");
+const dd = document.getElementsByClassName("dd");
+const du = document.getElementsByClassName("du");
 
 function myFunction(x) {
 	for (var i = 0; i < mnav.length; i++) {
@@ -9,3 +11,13 @@ function myFunction(x) {
 	x.classList.toggle("change");
 }
 
+function showmarvel(){
+	for (var i = 0; i < marvel.length; i++) {
+		marvel[i].classList.toggle("open");
+	}
+	for (var i = 0; i < dd.length; i++) {
+		dd[i].classList.toggle("open");
+		du[i].classList.toggle("open");
+	}
+
+}
